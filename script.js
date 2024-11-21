8cad4512bece7b6637bbace6fb0bc67dbf68f0b5
Iflaq khurshid <112414810+Iflaqbhat@users.noreply.github.com>
2024-11-21 10:22:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,11 +12,13 @@ function Employee(name, age, jobTitle) {
   this.jobTitle = jobTitle;    // Initialize jobTitle
 }
 
-
+// Set up inheritance
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
 
 Employee.prototype.jobGreet = function () {
   console.log(
-    `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
+    `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`
   );
 };
 
